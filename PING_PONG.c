6984acaf46652c193468c83802ec56d476832be8
Harshit Behal <harshit1behal@gmail.com>
2024-08-30 14:22:00 +0000

    library inclusion

---
+++ PING_PONG.c
@@ -0,0 +1,6 @@
+#include <graphics.h>  // Used for handling graphical operations like drawing shapes, lines, and images.
+#include <stdio.h>     // Standard input/output library used for handling I/O operations. 
+#include <stdlib.h>    // Provides functions for dynamic memory allocation (malloc, free), process control (exit), conversions, and other utilities like random number generation (rand).
+#include <conio.h>     // Console input/output library, mainly for Turbo C/C++ environments.
+#include <dos.h>       // Used for low-level system functions related to the Disk Operating System (DOS).
+#include <time.h>      // Time library used for handling time-related operations such as measuring time (clock), managing dates, and delays based on real-time values.
